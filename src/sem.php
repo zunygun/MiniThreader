b5dcb48049be9a782f02ef19ea2f2fd1f0c2ce6d
wapmorgan <wapmorgan@gmail.com>
2015-07-21 01:16:35 +0300

    Update

--- src/sem.php
+++ src/sem.php
@@ -10,7 +10,7 @@ if (!function_exists('ftok')) {
     }
 }
 
-if ( !extension_loaded('sem') ) {
+if (!function_exists('sem_get')) {
     function sem_get($key) { return fopen(sys_get_temp_dir().'/php.sem.'.$key, 'w+'); }
     function sem_acquire($sem_id) { return flock($sem_id, LOCK_EX); }
     function sem_release($sem_id) { return flock($sem_id, LOCK_UN); }

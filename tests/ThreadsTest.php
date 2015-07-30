8db468c1d7e6ee2cb2c38a4c5e3212a8c29d2507
wapmorgan <wapmorgan@gmail.com>
2015-07-30 22:34:50 +0300

    Add tests

---
+++ tests/ThreadsTest.php
@@ -0,0 +1,31 @@
+<?php
+use wapmorgan\MiniThreader\Threads;
+
+class ThreadsTest extends PHPUnit_Framework_TestCase {
+    public function testSimple() {
+        $threads = new Threads(function (Threads $threads) {
+            $data = $threads->storage->retrieveAndLock();
+            $data[] = getmypid();
+            $threads->storage->storeAndUnlock($data);
+        });
+        $threads->useCommonStorage();
+        $threads->setThreadsNumber(rand(2, 4));
+        $children = $threads->runThreads();
+
+        $running = $children;
+        while (count($running) > 0) {
+            foreach ($running as $i => $thread) {
+                if (!$thread->stillWorking())
+                    unset($running[$i]);
+            }
+            usleep(200);
+        }
+        $result = $threads->storage->retrieve():
+
+        $pids = array();
+        foreach ($children as $child) {
+            $this->assertContains($child->getPid(), $result);
+        }
+    }
+
+}
